30e3d9148275fbf300e80ad91e4b4a84dba1524d
Serdna20 <afgarciasa@ufpso.edu.co>
2024-05-19 23:58:30 -0500

    Small ui changes

---
+++ src/components/vueTesting/TestButton.vue
@@ -0,0 +1,33 @@
+<script setup>
+    import { ref } from 'vue'
+
+    defineProps({
+        title: String,
+        information: String
+    })
+
+    let buttonStatus = ref(false)
+    let buttonText = ref("Mostrar")
+
+    function showHideText() {
+        buttonStatus.value = ! buttonStatus.value
+
+        buttonText.value = "Mostrar"
+        if (buttonStatus.value) {
+            buttonText.value = "Ocultar"
+        }
+    }
+
+</script>
+
+<template>
+    <div class="mx-auto max-w-96 w-full max-h-96 h-full bg-slate-700">
+        <section class="border-b border-b-white flex flex-row justify-around">
+            <h1>{{ title }}</h1>
+            <button @click="showHideText" class="w-32 bg-white border rounded">{{ buttonText }}</button>
+        </section>
+        <section v-if="buttonStatus">
+            <h1>{{ information }}</h1>
+        </section>
+    </div>
+</template>
